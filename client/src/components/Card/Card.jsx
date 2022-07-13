61d14933fbf68caba2333fa8515153197812d9ed
Naman13Kumawat <naman.kumawat07@gmail.com>
2022-07-13 14:44:16 +0530

    Add Profile and Card responsiveness

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -8,20 +8,23 @@ export default function Card() {
         alt="postpicture"
         className="postImg"
       />
-      <h1>How to create awesome 3D model with Blender</h1>
-      <p>
-        In this article, we'll learn how to create a 3D model with blender only.
-      </p>
-      <div className="user">
-        <img
-          src="https://www.nicepng.com/png/full/182-1829287_cammy-lin-ux-designer-circle-picture-profile-girl.png"
-          alt="profilePic"
-        />
+      <div className="card_body">
+        <h1>How to create awesome 3D model with Blender</h1>
         <p>
-          by <b>Budi Supriadi</b>&nbsp;&nbsp;&nbsp;
-          <span className="circle">&nbsp;&nbsp;&nbsp;</span>&nbsp;&nbsp;&nbsp; 7
-          min read
+          In this article, we'll learn how to create a 3D model with blender
+          only.
         </p>
+        <div className="user">
+          <img
+            src="https://www.nicepng.com/png/full/182-1829287_cammy-lin-ux-designer-circle-picture-profile-girl.png"
+            alt="profilePic"
+          />
+          <p>
+            by <b>Budi Supriadi</b>&nbsp;&nbsp;&nbsp;
+            <span className="circle">&nbsp;&nbsp;&nbsp;</span>&nbsp;&nbsp;&nbsp;
+            7 min read
+          </p>
+        </div>
       </div>
     </div>
   );
